7bcaf49c38702b7146fe3ce95dbf435cb4f55dbe
lorena lima <131000794+lorenazx@users.noreply.github.com>
2023-10-20 11:06:41 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+functiontocasom(seletoraudio){
+const elemento=document querySelector (selectoraudio)
+IF (elements&&elementolocal(name===áudio){
+  elemento.play();
+}
+  else{
+    console.log(elementonaoencontradoouseletorinvalido)
+  }
+}
+const listadeteclas=document queryselectorall(tecla)
+for(let.contador=O;contador++){
+  const tecla=listadeteclas[contador];
+  const instrumento=tecla.classlist[1]
+  
